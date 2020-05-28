c6fb5cdc042f73176443128b65599ab3c890d36f
naresh <agrawal.nar@northeastern.edu>
2020-05-28 16:11:09 -0400

    adding webapp

---
+++ src/test/java/com/csye6225/webapps/WebappsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csye6225.webapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
