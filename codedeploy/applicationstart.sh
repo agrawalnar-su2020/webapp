872ba4fdf8ace63cd52382e59c33834b36e06f77
naresh <agrawal.nar@northeastern.edu>
2020-06-27 19:44:20 -0400

    a6_test8

--- codedeploy/applicationstart.sh
+++ codedeploy/applicationstart.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-echo "#!/bin/bash" > /tmp/tempFile.sh
-echo "cd /var" >> /tmp/tempFile.sh
-echo "sudo java -jar webapps-0.0.1-SNAPSHOT.war" >> /tmp/tempFile.sh
-cd /tmp/
-./tempFile.sh > run.out 2> run.err &
+cd /var
+sudo java -jar webapps-0.0.1-SNAPSHOT.war &
