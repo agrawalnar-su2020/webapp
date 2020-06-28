ed86872b5b01b9b19bb2db23271a2a60f34a824b
naresh <agrawal.nar@northeastern.edu>
2020-06-28 09:54:34 -0400

    beforeinstall

--- codedeploy/beforeinstall.sh
+++ codedeploy/beforeinstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo kill -9 $(lsof -t -i:8080)
-sudo kill -9 $(lsof -t -i:8080)
-sudo rm -rf /var/webapps-0.0.1-SNAPSHOT.war
+sudo kill -9 $(sudo lsof -t -i:8080)
+sudo kill -9 $(sudo lsof -t -i:8080)
+cd /var
+sudo rm -f webapps-0.0.1-SNAPSHOT.war
+cd ~
 sleep 10
