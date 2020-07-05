c2ebdabc3071e009bebf0d449c5e354654be5fea
naresh <agrawal.nar@northeastern.edu>
2020-07-05 14:48:49 -0400

    a7_test2

--- codedeploy/afterinstall.sh
+++ codedeploy/afterinstall.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-    -a fetch-config \
-    -m ec2 \
-    -c file:/opt/cloudwatch-config.json \
-    -s
 sudo systemctl restart amazon-cloudwatch-agent
-sleep 10
+cd /var
+sudo java -jar webapps-0.0.1-SNAPSHOT.war > /dev/null 2> /dev/null < /dev/null &
+sleep 240
--- codedeploy/applicationstart.sh
+++ codedeploy/applicationstart.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cd /var
-sudo java -jar webapps-0.0.1-SNAPSHOT.war > /dev/null 2> /dev/null < /dev/null &
-sleep 240
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/opt/cloudwatch-config.json \
+    -s
+sleep 10
