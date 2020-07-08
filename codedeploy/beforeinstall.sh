ab160ea08af9d4f40a4549dd762ed0a0883f29e7
naresh <agrawal.nar@northeastern.edu>
2020-07-08 13:50:06 -0400

    reboot_test3

--- codedeploy/beforeinstall.sh
+++ codedeploy/beforeinstall.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-sudo kill -9 $(sudo lsof -t -i:8080)
-sudo kill -9 $(sudo lsof -t -i:8080)
+# sudo kill -9 $(sudo lsof -t -i:8080)
+# sudo kill -9 $(sudo lsof -t -i:8080)
+PID=`ps -C java -o pid=`
+kill -9 $PID
 cd /var
 sudo rm -f webapps-0.0.1-SNAPSHOT.war
 sudo rm -f webapps.log
