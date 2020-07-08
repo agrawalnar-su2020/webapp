110829f50c91428069d7437b40d22b23f1f0ec1a
naresh <agrawal.nar@northeastern.edu>
2020-07-08 18:36:36 -0400

    reboot_test5

--- codedeploy/afterinstall.sh
+++ codedeploy/afterinstall.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+sudo cp /var/restartwebapp.service /etc/systemd/system/
+sudo chmod +x /etc/systemd/system/restartwebapp.service
+sudo systemctl daemon-reload
+sudo systemctl enable restartwebapp.service
+sudo systemctl restart restartwebapp.service
+
 cd /var
-nohup java -jar webapps-0.0.1-SNAPSHOT.war 1> /home/ubuntu/applogs/webapp.out 2>&1 </dev/null &
+sudo java -jar webapps-0.0.1-SNAPSHOT.war > /dev/null 2> /dev/null < /dev/null &
 cd ~
 sleep 240
+sudo systemctl restart amazon-cloudwatch-agent
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/opt/amazon-cloudwatch-agent.json \
+    -s
--- codedeploy/beforeinstall.sh
+++ codedeploy/beforeinstall.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# sudo kill -9 $(sudo lsof -t -i:8080)
-# sudo kill -9 $(sudo lsof -t -i:8080)
-PID=`ps -C java -o pid=`
-kill -9 $PID
+sudo kill -9 $(sudo lsof -t -i:8080)
+sudo kill -9 $(sudo lsof -t -i:8080)
 cd /var
 sudo rm -f webapps-0.0.1-SNAPSHOT.war
 sudo rm -f webapps.log
