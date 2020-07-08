3733c8a334320d2b3974044528a45d6065bba554
naresh <agrawal.nar@northeastern.edu>
2020-07-08 11:44:21 -0400

    reboot_test1

--- codedeploy/afterinstall.sh
+++ codedeploy/afterinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /var
-sudo java -jar webapps-0.0.1-SNAPSHOT.war > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar webapps-0.0.1-SNAPSHOT.war > /dev/null 2> /dev/null < /dev/null &
 cd ~
 sleep 240
