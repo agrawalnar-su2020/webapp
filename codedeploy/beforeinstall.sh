778a64d2589e8f4db58b87b53006e207b96db676
naresh <agrawal.nar@northeastern.edu>
2020-07-08 20:34:51 -0400

    reboot_test8

--- codedeploy/beforeinstall.sh
+++ codedeploy/beforeinstall.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-sudo cp /var/restartwebapp.service /etc/systemd/system/
-sudo chmod +x /etc/systemd/system/restartwebapp.service
-sudo systemctl daemon-reload
-sudo systemctl enable restartwebapp.service
-sudo systemctl restart restartwebapp.service
-sudo kill -9 $(sudo lsof -t -i:8080)
-sudo kill -9 $(sudo lsof -t -i:8080)
+set -x
 cd /var
-sudo chmod 755 afterinstall.sh
 sudo rm -f webapps-0.0.1-SNAPSHOT.war
 sudo rm -f webapps.log
 cd ~
-sleep 10
+sleep 5
+set +x
