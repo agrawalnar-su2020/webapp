bea3dd01ed1ca22ab6f6ab138f8e9557041a5bd6
naresh <agrawal.nar@northeastern.edu>
2020-07-08 23:36:14 -0400

    reboot_test10

--- codedeploy/afterinstall.sh
+++ codedeploy/afterinstall.sh
@@ -5,7 +5,6 @@ sudo cp /var/restartwebapp.service /etc/systemd/system/
 sudo chmod +x /etc/systemd/system/restartwebapp.service
 sudo systemctl daemon-reload
 sudo systemctl enable restartwebapp.service
-sudo systemctl restart restartwebapp.service
 sudo kill -9 $(sudo lsof -t -i:8080)
 sudo kill -9 $(sudo lsof -t -i:8080)
 set +x
